79977e6ef4b417dadb1cc182e9225af1f57bc241
Tony Liu <tony.liu404@gmail.com>
2026-03-26 14:35:39 -0400

    edited main.py to run server

--- backend/main.py
+++ backend/main.py
@@ -328,7 +328,7 @@ def test(img_path: Optional[str] = None):
         print(f"{img_path} does not exist")
 
 if __name__ == "__main__":
-    main()
-    # port = int(os.environ.get("PORT", 8000))
-    # import uvicorn
-    # uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True) #uses api.py
+    # main()
+    port = int(os.environ.get("PORT", 8000))
+    import uvicorn
+    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=True) #uses api.py
